8e17ee962f1f66d1e0dba1060a646a7b167f2043
Sai Adithya Kothamasu <adithya261004@gmail.com>
2024-11-01 20:01:05 +0530

    fix: routing configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [];
+  }
 };
 
 export default nextConfig;
