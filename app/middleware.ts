1a097c8f46bb08a17015e3bbf807b5bfe37ce60d
Sai Adithya Kothamasu <adithya261004@gmail.com>
2024-11-01 18:31:35 +0530

    middleware updated to redirect to landing page for all

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,21 +1,9 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(req: NextRequest) {
   console.log('Request URL:', req.url);
 
-  // Avoid redirect loop by skipping middleware for specific paths
-  if (req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/api')) {
-    console.log('Skipping middleware for:', req.nextUrl.pathname);
-    return NextResponse.next();
-  }
-
-  try {
-    return clerkMiddleware(req, {} as NextFetchEvent);
-  } catch (error) {
-    console.error('Error in clerkMiddleware:', error);
-    return NextResponse.redirect(new URL('/error', req.url));
-  }
+    return NextResponse.redirect(new URL('/', req.url));
 }
 
 export const config = {
